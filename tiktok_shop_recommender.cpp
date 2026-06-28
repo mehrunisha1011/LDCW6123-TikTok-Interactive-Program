dff000397cc1dd32763a8a793abe70aa6861ecfa
Mehrunisha <mehrunisha1011@gmail.com>
2026-06-28 22:51:12 +0800

    Added closing message
    Improved user experience with friendly closing message

--- tiktok_shop_recommender.cpp
+++ tiktok_shop_recommender.cpp
@@ -111,6 +111,7 @@ int main() {
     }
  
     cout << "Thank you for using TikTok Shop Recommendation Assistant!" << endl;
+    cout << "Have a great day shopping on TikTok Shop!" << endl;
  
     return 0;
 }
